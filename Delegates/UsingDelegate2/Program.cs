7dcb00562a3ae1a908ca70388b7ad3d275597a18
Michael Mathu <mathumichael17@gmail.com>
2022-06-19 10:04:44 +0300

    added car delegate repo

---
+++ Delegates/UsingDelegate2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using SimpleDelegate;
+
+
+
+    // Create a BinaryOp delegate object that
+    // "points to" SimpleMath.Add().
+    BinaryOp b = new BinaryOp(SimpleMath.Add);
+
+    // Invoke Add() method indirectly using delegate object.
+    Console.WriteLine("10 + 10 is {0}", b(10, 10));
+    Console.ReadLine();
+
+    //Additional type definitions must be placed at the end of the
+    // top-level statements
+    // This delegate can point to any method,
+    // taking two integers and returning an integer.
+    public delegate int BinaryOp(int x, int y);
+
+
+
+
