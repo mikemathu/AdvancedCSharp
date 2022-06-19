804adc2eda67dadb661aef0c8094859da6f959f8
Michael Mathu <mathumichael17@gmail.com>
2022-06-19 10:41:53 +0300

    car delegate

---
+++ Delegates/CarDelegate/Car.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDelegate
+{
+    public class Car
+    {
+        public int CurrentSpeed { get; set; }
+        public int MaxSpeed { get; set; } = 100;
+        public string PetName { get; set; }
+
+        // Is the car alive or dead?
+        private bool _carIsDead;
+
+        // Class constructors.
+        public Car() { }
+        public Car(string name, int maxSp, int currSp)
+        {
+            CurrentSpeed = currSp;
+            MaxSpeed = maxSp;
+            PetName = name;
+        }
+
+        //1) Define a delagate type
+        public delegate void CarEngineHandler(string msgForCaller);
+
+        //2) Define a member variable of this delegate.
+        private CarEngineHandler _listOfHandlers;
+
+        // 3) Add registration function for the caller.
+        public void RegisterWithCarEngine(CarEngineHandler methodToCall)
+        {
+            _listOfHandlers = methodToCall;
+        }
+
+
+
+        // 4) Implement the Accelerate() method to invoke the delegate's
+        // invocation list under the correct circumstances.
+        public void Accelerate(int delta)
+        {
+            // If this car is "dead," send dead message.
+            if (_carIsDead)
+            {
+                _listOfHandlers?.Invoke("Sorry, this car is dead...");
+            }
+            else
+            {
+                CurrentSpeed += delta;
+                // Is this car "almost dead"?
+                if (10 == (MaxSpeed - CurrentSpeed))
+                {
+                    _listOfHandlers?.Invoke("Careful buddy! Gonna blow!");
+                }
+                if (CurrentSpeed >= MaxSpeed)
+                {
+                    _carIsDead = true;
+                }
+                else
+                {
+                    Console.WriteLine("CurrentSpeed = {0}", CurrentSpeed);
+                }
+            }
+        }
+
+
+
+
+
+
+            }
+    }
--- Delegates/CarDelegate/Program.cs
+++ Delegates/CarDelegate/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarDelegate;
+
+// First, make a Car object.
+Car c1 = new Car("SlugBug", 100, 10);
+
+// Now, tell the car which method to call
+// when it wants to send us messages.
+c1.RegisterWithCarEngine(
+ new Car.CarEngineHandler(OnCarEngineEvent));
+
+// Speed up (this will trigger th events).
+Console.WriteLine("***** Speeding up *****");
+for (int i = 0; i < 6; i++)
+{
+    c1.Accelerate(20);
+}
+Console.ReadLine();
+
+// This is the target for incoming events.
+static void OnCarEngineEvent(string msg)
+{
+    Console.WriteLine("\n*** Message From Car Object ***");
+    Console.WriteLine("=> {0}", msg);
+    Console.WriteLine("********************\n");
+}
